49eddfa000a8d02c67f5d8ab729bb5b965f99dfc
kiinshuk <red2k2green@gmail.com>
2026-03-16 15:01:07 +0530

    Add environment variables to WSGI for PythonAnywhere

--- snapgram/wsgi.py
+++ snapgram/wsgi.py
@@ -11,12 +11,24 @@ import os
 import sys
 
 # Add project directory to Python path
-path = '/home/kiinshuk/snapgram'
+path = '/home/kiinshuk/sg'
 if path not in sys.path:
     sys.path.insert(0, path)
 
-from django.core.wsgi import get_wsgi_application
-
+# Environment variables for PythonAnywhere
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snapgram.settings')
+os.environ['SECRET_KEY'] = 'YOUR-SECRET-KEY-HERE'
+os.environ['DEBUG'] = 'False'
+os.environ['MYSQL_DB'] = '1'
+os.environ['MYSQL_NAME'] = 'kiinshuk$snapgram'
+os.environ['MYSQL_USER'] = 'kiinshuk'
+os.environ['MYSQL_PASSWORD'] = 'YOUR-MYSQL-PASSWORD'
+os.environ['MYSQL_HOST'] = 'mysql.pythonanywhere-services.com'
+os.environ['MYSQL_PORT'] = '3306'
+os.environ['CLOUDINARY_CLOUD_NAME'] = 'YOUR-CLOUD-NAME'
+os.environ['CLOUDINARY_API_KEY'] = 'YOUR-API-KEY'
+os.environ['CLOUDINARY_API_SECRET'] = 'YOUR-API-SECRET'
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
